adaea3edad8537bb71a630728b7537f15b1d130d
SevernE <SevernE@lucanet.com>
2017-12-27 11:36:14 +0100

    Initial commit for base project

---
+++ PackratConfigServer/src/main/java/com/lucanet/packratconfigserver/PackratConfigServer.java
@@ -0,0 +1,9 @@
+package com.lucanet.packratconfigserver;
+
+public class PackratConfigServer {
+
+  public static void main(String[] args) {
+    System.out.println("PACKRAT CONFIG SERVER");
+  }
+
+}
---
+++ PackratReporter/src/main/java/com/lucanet/packratreporter/PackratReporter.java
@@ -0,0 +1,9 @@
+package com.lucanet.packratreporter;
+
+public class PackratReporter {
+
+  public static void main(String[] args) {
+    System.out.println("PACKRAT REPORTER");
+  }
+
+}
