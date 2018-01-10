31c0dac3926ad13eb906bfdfca69f453104bf67a
SevernE <SevernE@lucanet.com>
2018-01-10 08:07:00 +0100

    Split up "logExit" aspect to log function exits with a return value and function exits with a thrown exception

--- PackratCommon/src/main/java/com/lucanet/packratcommon/aspects/LogExecutionAspect.java
+++ PackratCommon/src/main/java/com/lucanet/packratcommon/aspects/LogExecutionAspect.java
@@ -1,9 +1,7 @@
 package com.lucanet.packratcommon.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -35,18 +33,37 @@ public class LogExecutionAspect {
   public void logBefore(JoinPoint joinPoint) {
     String targetClassName = joinPoint.getTarget().getClass().getName();
     Logger logger = LoggerFactory.getLogger(targetClassName);
-    logger.trace("Entering: {}:{}", targetClassName, joinPoint.getSignature().getName());
+    logger.trace("Entering {}:{}", targetClassName, joinPoint.getSignature().getName());
   }
 
   /**
-   * Log an exit into the annotated function for trace reporting purposes.
+   * Log an exit out of the annotated function for with a return value for trace reporting purposes.
    * @param joinPoint The function that the aspect will log as exiting.
+   * @param returnValue The value that the function returned.
    */
-  @After("@annotation(LogExecution)")
-  public void logAfter(JoinPoint joinPoint) {
+  @AfterReturning(
+      value = "@annotation(LogExecution)",
+      returning = "returnValue"
+  )
+  public void logAfterReturning(JoinPoint joinPoint, Object returnValue) {
     String targetClassName = joinPoint.getTarget().getClass().getName();
     Logger logger = LoggerFactory.getLogger(targetClassName);
-    logger.trace("Exiting: {}:{}", targetClassName, joinPoint.getSignature().getName());
+    logger.trace("Exiting {}:{} with a return of {} | value '{}'", targetClassName, joinPoint.getSignature().getName(), returnValue.getClass().toString(), returnValue);
+  }
+
+  /**
+   * Log an exit out of the annotated function with a thrown exception for trace reporting purposes.
+   * @param joinPoint The function that the aspect will log as exiting.
+   * @param thrownException The exception that the function threw.
+   */
+  @AfterThrowing(
+      value = "@annotation(LogExecution)",
+      throwing = "thrownException"
+  )
+  public void logAfterThrowing(JoinPoint joinPoint, Throwable thrownException) {
+    String targetClassName = joinPoint.getTarget().getClass().getName();
+    Logger logger = LoggerFactory.getLogger(targetClassName);
+    logger.trace("Exiting {}:{} throwing {}", targetClassName, joinPoint.getSignature().getName(), thrownException);
   }
 
   // ========================== Protected Methods ==========================79
